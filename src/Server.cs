765897bf7df0c3ff600c3d3cfb9eb61ce780c0a9
Anton Minko <anton.minko@stash.com>
2024-11-18 14:47:40 +0000

    fix while condition

--- src/Server.cs
+++ src/Server.cs
@@ -11,12 +11,12 @@ server.Start();
 try
 {
     var socket = await server.AcceptSocketAsync(); // wait for client
-    while (true) {
+    while (socket.Connected) {
         var buffer = new byte[1024];
         var received = await socket.ReceiveAsync(buffer, SocketFlags.None);
         var requestPayload = Encoding.UTF8.GetString(buffer, 0, received);
         Console.WriteLine($"Received request: {requestPayload}");
-        
+
         await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
     }
 }
