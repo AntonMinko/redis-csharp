46bac04006ef19ed7dab545c1f76ab97479ee354
Anton Minko <anton.minko@stash.com>
2024-11-18 14:46:41 +0000

    handle multiple requests

--- src/Server.cs
+++ src/Server.cs
@@ -8,8 +8,20 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-while (true) {
+try
+{
     var socket = await server.AcceptSocketAsync(); // wait for client
-    Console.WriteLine("Client connected, responding PONG");
-    await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+    while (true) {
+        var buffer = new byte[1024];
+        var received = await socket.ReceiveAsync(buffer, SocketFlags.None);
+        var requestPayload = Encoding.UTF8.GetString(buffer, 0, received);
+        Console.WriteLine($"Received request: {requestPayload}");
+        
+        await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+    }
 }
+finally
+{
+    server.Stop();
+}
+
