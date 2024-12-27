e63585e37ba7cd9a417ea595abfbcf356ec66394
antonminko <minko.anton@gmail.com>
2024-12-27 19:06:09 +0000

    codecrafters submit [skip ci]

---
+++ src/CommandParser.cs
@@ -0,0 +1,16 @@
+namespace codecrafters_redis;
+
+public static class CommandParser
+{
+    public static List<string> Parse(this string command)
+    {
+        var result = new List<string>();
+        var parts = command.Split("\r\n").Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+        for (int i = 2; i < parts.Count; i += 2)
+        {
+            result.Add(parts[i]);
+        }
+        
+        return result;
+    }
+}
--- src/TcpConnectionWorker.cs
+++ src/TcpConnectionWorker.cs
@@ -1,9 +1,10 @@
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Unicode;
 
 namespace codecrafters_redis;
 
-public class TcpConnectionWorker(Socket socket)
+public class TcpConnectionWorker(Socket socket) : IDisposable
 {
     public async Task HandleConnectionAsync()
     {
@@ -19,8 +20,9 @@ public class TcpConnectionWorker(Socket socket)
                 }
                 var requestPayload = Encoding.UTF8.GetString(buffer, 0, received);
                 Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}. Received request: {requestPayload}");
-
-                await socket.SendAsync("+PONG\r\n"u8.ToArray(), SocketFlags.None);
+                
+                var command = requestPayload.Parse();
+                await ProcessCommandAsync(socket, command);
             }
             
             socket.Close();
@@ -32,6 +34,33 @@ public class TcpConnectionWorker(Socket socket)
         }
     }
 
+    private async Task ProcessCommandAsync(Socket socket, List<string> command)
+    {
+        switch (command[0].ToUpperInvariant())
+        {
+            case "PING":
+                await socket.SendAsync(SimpleString("PONG"), SocketFlags.None);
+                break;
+            case "ECHO":
+                await socket.SendAsync(SimpleString(command[1]), SocketFlags.None);
+                break;
+            default:
+                Console.WriteLine("Unknown command: " + String.Join(" ", command));
+                await socket.SendAsync(ErrorString($"Unknown command {command[0]}"), SocketFlags.None);
+                break;
+        }
+    }
+
+    private byte[] SimpleString(string s)
+    {
+        return Encoding.UTF8.GetBytes($"+{s}\r\n");
+    }
+
+    private byte[] ErrorString(string message)
+    {
+        return Encoding.UTF8.GetBytes($"-{message}\r\n");
+    }
+    
     public void Dispose()
     {
         socket.Dispose();
