5988800147f42baf06fafd19ec9995bef2287839
antonminko <minko.anton@gmail.com>
2025-10-15 21:20:07 +0100

    codecrafters submit [skip ci]

--- src/Commands/Handlers/LPop.cs
+++ src/Commands/Handlers/LPop.cs
@@ -2,7 +2,7 @@ using codecrafters_redis.Commands.Handlers.Validation;
 
 namespace codecrafters_redis.Commands.Handlers;
 
-[Arguments(Min = 1, Max = 1)]
+[Arguments(Min = 1, Max = 2)]
 [Supports(StorageType = ValueType.StringArray)]
 internal class LPop(IStorage storage, Settings settings) : BaseHandler(settings)
 {
@@ -12,14 +12,22 @@ internal class LPop(IStorage storage, Settings settings) : BaseHandler(settings)
     protected override Task<RedisValue> HandleSpecific(Command command, ClientConnection connection)
     {
         var key = command.Arguments[0];
+        bool hasCountArg = command.Arguments.Length == 2;
+        var count = hasCountArg ? int.Parse(command.Arguments[1]) : 1;
 
         var typedValue = storage.Get(key);
         if (typedValue == null) return Task.FromResult(EmptyBulkStringArray);
         if (!ValidateValueType(typedValue, out var error)) return Task.FromResult(error!);
 
         var list = typedValue.Value.GetAsStringList();
-        var element = list.First();
-        list.RemoveFirst();
+        var removedItems = new List<string>();
+        while (count > 0 && list.Count > 0)
+        {
+            removedItems.Add(list.First());
+            list.RemoveFirst();
+            count--;
+        }
+        
         if (list.Count == 0)
         {
             storage.Remove(key);
@@ -28,7 +36,9 @@ internal class LPop(IStorage storage, Settings settings) : BaseHandler(settings)
         {
             storage.Set(key, typedValue.Value);
         }
-        
-        return Task.FromResult(element.ToBulkString());
+
+        return hasCountArg
+            ? Task.FromResult(removedItems.ToBulkStringArray())
+            : Task.FromResult(removedItems[0].ToBulkString());
     }
 }
